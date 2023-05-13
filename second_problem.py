dde08d7747825dc56aa439fc7ff1361e2aba7e5f
mthmai <matheuspereiramai@gmail.com>
2023-05-13 20:17:59 -0300

    Create second problem from Rosalind

---
+++ second_problem.py
@@ -0,0 +1,18 @@
+def read_file(path: str) -> str:
+    with open(path, "r") as file_seq:
+        seq = file_seq.read().replace('\n', '')
+
+    return seq
+
+def seq_RNA(seq: str) -> str:
+
+    sequencia_DNA = seq.lower()
+    
+    sequencia_RNA = sequencia_DNA.replace('t', 'u')
+    return sequencia_RNA.upper()
+
+
+if __name__ == '__main__':
+
+   seq = read_file('/home/ubuntu/Rosalind_problems/seq_rosalind.txt')
+   print('Aqui está o RNA: ', seq_RNA(seq))
