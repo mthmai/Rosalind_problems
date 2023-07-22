266f0c10d479145d479955b10593a4d3944be49d
mthmai <matheuspereiramai@gmail.com>
2023-07-21 21:15:22 -0300

    Terminando a terceira atividade

--- three_problem.py
+++ three_problem.py
@@ -1,8 +1,42 @@
+from typing import List
+
 def read_file(path: str) -> str:
     with open(path, "r") as file_seq:
         seq = file_seq.read().replace('\n', '')
 
     return seq
 
-def complement_DNA(seq : str) -> str:
+
+def DNA_to_list(seq : str) -> List[str]:
+
+    list_seq = list()
+    seq = seq.upper()
+    for letter in seq:
+        list_seq.append(letter)
+
+    return list_seq
+
+
+def DNA_complement(list_seq: List[str]) -> str:
+
+    list_seq_complement = list()
+    for letter in list_seq:
+        if letter == 'A':
+            list_seq_complement.append('T')
+        elif letter == 'T':
+            list_seq_complement.append('A')
+        elif letter == 'C':
+            list_seq_complement.append('G')
+        elif letter == 'G':
+            list_seq_complement.append('C')
+        else:
+            raise ValueError(f'A letra {letter} não é nucleotídeo')
     
+    return list_seq_complement
+
+
+if __name__ == '__main__':
+
+    #seq_DNA = read_file('')
+    seq = DNA_to_list('atcyg')
+    print(DNA_complement(seq))
